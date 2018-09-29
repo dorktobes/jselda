7f9dece9f01e877a3b92a4d532d29b7f2e3b2364
Toby Hoshman <t.hoshman@gmail.com>
2018-09-29 12:33:18 -0700

    add Dungeon class

---
+++ class/Dungeon/index.js
@@ -0,0 +1,19 @@
+/* Dungeon class should:
+  have a matrix representing the dungeon layout
+  have all of the room tiles
+  have knowledge of which room the player character is in
+  be able to move the player character
+
+*/
+
+export default class Dungeon {
+  constructor() {
+    // this will be unique for every dungeon subclass
+    this.playerLocation = [];
+    this.layout = [];
+    this.tiles = {};
+  }
+  movePC(newLocation) {
+    this.playerLocation = newLocation;
+  }
+}
